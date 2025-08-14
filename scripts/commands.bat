566694ac37717a96bc971c7dd864562352b9f6f9
David-396 <david.eini12@gmail.com>
2025-08-14 16:49:11 +0300

    add the mysql-svc yaml

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -7,7 +7,4 @@ oc apply -f C:\Users\User\Desktop\OpenShift-DataLoader\infrastructure\k8s\mysql-
 # creating the new mysql app
 oc new-app mysql:latest --env=MYSQL_USER=david --env=MYSQL_PASSWORD=pwd  --env=MYSQL_DATABASE=DataLoaderDB
 
-# creating the pvc
-oc apply -f .\infrastructure\k8s\mysql-pvc.yaml
-
 
