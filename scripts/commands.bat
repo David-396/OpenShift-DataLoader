ba07dce0c6d26d97123bb6591411b1e2589c5a0c
David-396 <david.eini12@gmail.com>
2025-08-14 11:35:14 +0300

    create the mysql-pv

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,2 +1,10 @@
- oc new-app mysql:latest
+# creating the secret
+oc apply -f C:\Users\User\Desktop\OpenShift-DataLoader\infrastructure\k8s\mysql-secret.yaml
+
+# creating the Persistent Volume
+oc apply -f mysql-pv.yaml
+
+# creating the Persistent Volume Claim
+oc apply -f mysql-pvc.yaml
+
 
