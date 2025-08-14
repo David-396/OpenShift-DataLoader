f54d13128d7e46a2cf1f6823e6e2b6b197dc1a1c
David-396 <david.eini12@gmail.com>
2025-08-14 14:31:34 +0300

    create the new deployment

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,10 +1,7 @@
 # creating the secret
 oc apply -f C:\Users\User\Desktop\OpenShift-DataLoader\infrastructure\k8s\mysql-secret.yaml
 
-# creating the Persistent Volume
-oc apply -f mysql-pv.yaml
-
 # creating the Persistent Volume Claim
-oc apply -f mysql-pvc.yaml
+oc apply -f C:\Users\User\Desktop\OpenShift-DataLoader\infrastructure\k8s\mysql-pvc.yaml
 
 
