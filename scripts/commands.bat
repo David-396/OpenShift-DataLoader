ac3449eb61bb0527e56a77ef6ce5138eaa37c690
David-396 <david.eini12@gmail.com>
2025-08-14 14:52:08 +0300

    add the pvc yaml

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -4,4 +4,10 @@ oc apply -f C:\Users\User\Desktop\OpenShift-DataLoader\infrastructure\k8s\mysql-
 # creating the Persistent Volume Claim
 oc apply -f C:\Users\User\Desktop\OpenShift-DataLoader\infrastructure\k8s\mysql-pvc.yaml
 
+# creating the new mysql app
+oc new-app mysql:latest --env=MYSQL_USER=david --env=MYSQL_PASSWORD=pwd  --env=MYSQL_DATABASE=DataLoaderDB
+
+# creating the pvc
+oc apply -f .\infrastructure\k8s\mysql-pvc.yaml
+
 
