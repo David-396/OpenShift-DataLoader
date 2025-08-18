441e072cfa72c6c82ef0957fc0fc61ebf7f895b5
David-396 <david.eini12@gmail.com>
2025-08-18 15:03:54 +0300

    finishing the project

--- services/data_loader/main.py
+++ services/data_loader/main.py
@@ -1,12 +1,20 @@
+import os
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
-from services.data_loader import data_loader
+import data_loader
 
-dal_obj = data_loader.DAL(database='DataLoaderDB',port=3306,host='localhost',user='david',password='pwd')
+dal_obj = data_loader.DAL(database=os.environ.get('MYSQL_DATABASE', 'DataLoaderDB'),
+                          port=os.environ.get('MYSQL_PORT', 3306),
+                          host=os.environ.get('MYSQL_HOST', 'localhost'),
+                          user=os.environ.get('MYSQL_USER', 'root'),
+                          password=os.environ.get('MYSQL_PASSWORD', 'pwd'))
+
+table_name = os.environ.get('TABLE_NAME', 'table1')
 
 app = FastAPI()
 
 @app.get('/get-data')
 def get_data():
-    data = dal_obj.flex_query('GET * FROM DataLoaderDB', return_=True)
+    query = f'SELECT * FROM {table_name};'
+    data = dal_obj.flex_query(query, return_=True)
     return JSONResponse(data)
